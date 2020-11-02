1e187d0539eccf6e4bc7a87b1da5244f2f9b2b94
Vukasin Macura <moj_email@gmail.com>
2020-11-02 21:56:30 +0100

    add register function in AuthController and add api route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,26 +38,27 @@ class AuthController extends Controller
         auth('api')->logout(true);
     }
 
-    // public function register(Request $request){
-    //     $validator = Validator::make($request->json()->all(), [
-    //             'name' => 'required|string|max:255',
-    //             'email' => 'required|string|email|max:255|unique:users',
-    //             'password' => 'required|string|min:6',
-    //         ]);
+    public function register(Request $request){
+        $validator = Validator::make($request->json()->all(), [
+                'first_name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
 
-    //         if($validator->fails()){
-    //             return response()->json($validator->errors()->toJson(), 400);
-    //         }
+            if($validator->fails()){
+                return response()->json($validator->errors()->toJson(), 400);
+            }
 
-    //         $user = User::create([
-    //             'name' => $request->json()->get('name'),
-    //             'email' => $request->json()->get('email'),
-    //             'password' => Hash::make($request->json()->get('password')),
-    //         ]);
+            $user = User::create([
+                'first_name' => $request->json()->get('first_name'),
+                'last_name' => $request->json()->get('last_name'),
+                'email' => $request->json()->get('email'),
+                'password' => Hash::make($request->json()->get('password')),
+            ]);
 
-    //         $token = JWTAuth::fromUser($user);
+            $token = JWTAuth::fromUser($user);
 
-    //         return response()->json(compact('user','token'), 201);
+            return response()->json(compact('user','token'), 201);
 
-    // }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
 Route::post('/logout', [AuthController::class, 'logout']);
-// Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register']);
